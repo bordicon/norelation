ed04bd2c69c8cb13af192c6828f427055452a903
Jesse Dhillon <jesse@devazero.com>
2010-12-31 03:05:08 +0000

    Added home controller

---
+++ no_relation/controllers/home.py
@@ -0,0 +1,12 @@
+import logging
+
+from pylons import request, response, session, tmpl_context as c, url
+from pylons.controllers.util import abort, redirect
+
+from no_relation.lib.base import BaseController, render
+
+log = logging.getLogger(__name__)
+
+class HomeController(BaseController):
+  def index(self):
+    return render('home_index.html')
